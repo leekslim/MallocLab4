47cceb0ac898e277b4a89482797210ce8760c5ac
leeks <lilim2019@u.northwestern.edu>
2018-06-12 15:09:22 -0500

    fixing textbook bugs

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -37,6 +37,7 @@ team_t team = {
 #define WSIZE 4 // size of the header and footer
 #define DSIZE 8 // regular or default block size
 #define CHUNKSIZE 512 // when extending heap
+#define MAX(x, y) ((x) > (y)? (x) : (y))
 
 /* pack size bit and allocated bit into same word */
 #define PACK(size, alloc)((size) | (alloc))
@@ -47,7 +48,7 @@ team_t team = {
 
 /* given addr p, read size bit or allocated bit */
 #define GET_SIZE(p)	(GET(p) & ~0x7) // the block size encapsulated in the first WSIZE-3 bits includes the payload, header and footer
-#define GET_ALL0C(p) (GET(p) & 0x1) // the alloc bit is the LSB of the header/footer
+#define GET_ALLOC(p) (GET(p) & 0x1) // the alloc bit is the LSB of the header/footer
 
 /* given block ptr bp, compute address of header or footer */
 #define HDRP(bp)	((char *)(bp) - WSIZE) //HDRP means HeaderPointer
@@ -64,91 +65,6 @@ team_t team = {
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 */
 
-
-/* 
- * mm_init - initialize the malloc package. A 'prologue' and 'epilogue' block is used to account for edge cases
- */
-int mm_init(void)
-{
-	/* create the initial empty heap, mem_sbrk returns a generic pointer to the start of the heap, so heap_listp currently holds it */
-	void *heap_listp = mem_sbrk(4*WSIZE)
-	if (heap_listp == (void *)–1) /* L: not entirely sure what this is checking for */
-		return –1;
-	PUT(heap_listp, 0);
-	PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
-	PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
-	PUT(heap_listp + (3*WSIZE), PACK(0, 1));
-	heap_listp += (2*WSIZE); /* changes local variable holding heap pointer to the start of the actual heap */
-	/* extend the empty heap with a free block of CHUNKSIZE bytes */
-	if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
-		return –1;
-    return 0;
-}
-
-/* 
- * mm_malloc - attemps to find a free block and then returns that pointer
- *
- */
-void *mm_malloc(size_t size)
-{
-	size_t asize;	/* adjusted block size */
-	size_t extendsize;	/* amount to extend heap if no fit */
-	char *bp;
-
-	/* ignore spurious requests */
-	if (size == 0)
-		return NULL;
-	
-	/* adjust block size to include overhead and alignment reqs. */
-	if (size <= DSIZE) /* smaller than regular block size of payload */
-		asize = 2*DSIZE;
-	else /* larger than regular block size of payload */
-	asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE); /*L: first DSIZE is for footer and header, second DSIZE-1 is for extra space if unaligned
-	its -1 because size is at least 1 above DSIZE, division mods away unaligned 'remainder', then multiply back */
-	/* Search the free list for a fit */
-	if ((bp = find_fit(asize)) != NULL) {
-		place(bp, asize);
-		return bp;
-	}
-
-	/* no fit found. get more memory and place the block */
-	extendsize = MAX(asize,CHUNKSIZE);
-	if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
-		return NULL;
-	place(bp, asize);
-	return bp;
-}
-
-/*
- * mm_free - frees, and then coalesces, L: I don't think it checks whether that block is free or not but I guess it's not necessary
- */
-void mm_free(void *ptr)
-{
-	size_t size = GET_SIZE(HDRP(bp));
-
-	PUT(HDRP(bp), PACKCsize, 0));
-	PUT(FTRPCbp), PACKCsize, 0));
-	coalesce(bp);
-}
-
-/*
- * mm_realloc - textbook does not have this implmentation, so we will have to work on it outselves
- */
-void *mm_realloc(void *ptr, size_t size)
-{
-    /*ss */
-}
-
-/*
-* L: Heap Checker as per instructions, should be called at various points to check heap
-*/
-
-int mm_check(void) {
-	int x=1; /*initialize non-zero value, should return 0 if error, and print error messages before that
-	all checking code should go here, printing error message if heap not consistent, and changing x to 0 */
-	return x;
-}
-
 /* PRIVATE STATIC FUNCTIONS */
 /* extends the heap, checks if enough memory available, and whether size requested is aligned */
 static void *extend_heap(size_t words)
@@ -158,13 +74,12 @@ static void *extend_heap(size_t words)
 	
 	/* allocate an even number of words to maintain alignment */
 	size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
-	if ((long)(bp = mem_sbrk(size)) == –1)
-		return NULL;
+	if ((long)(bp = mem_sbrk(size)) == –1){return NULL;}
 	
 	/* initialize free block header/footer and the epilogue header */
-	PUT(HDRP(bp), PACK(size, 0));		/* Free block header */
-	PUT(FTRP(bp), PACK(size, 0));		/* Free block footer */
-	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));	/* New epilogue header */
+	PUT(HDRP(bp), PACK(size, 0));		/* free block header */
+	PUT(FTRP(bp), PACK(size, 0));		/* free block footer */
+	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));	/* new epilogue header */
 	
 	/* coalesce if the previous block was free */
 	return coalesce(bp);
@@ -183,22 +98,22 @@ static void *coalesce(void *bp)
 
 	else if (prev_alloc && !next_alloc) {		/* Case 2: coalesce with next block */
 		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-		PUT(HDRP(bp), PACK(size, 0));
+		PUT (HDRP(bp), PACK(size,0));
 		PUT (FTRP(bp), PACK(size,0));
 	}
 
 	else if (!prev_alloc && next_alloc) {		/* Case 3: coalesce with prev block */
 		size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-		PUT(FTRPCbp), PACKCsize, 0));
-		PUT(HDRP(PREV_BLKP(bp)), PACKCsize, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
 		bp = PREV_BLKP(bp);
 	}
 
 	else {						/* Case 4: coalesce with both prev and next block*/
 		size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
-			GET_SIZE(FTRP(NEXT_BLKP(bp)));
-		PUT(HDRP(PREV_BLKP(bp)), PACKCsize, 0));
-		PUT(FTRP(NEXT_BLKP(bp)), PACKCsize, 0));
+				GET_SIZE(FTRP(NEXT_BLKP(bp)));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
 		bp = PREV_BLKP(bp);
 	}
 	return bp;
@@ -208,15 +123,13 @@ static void *coalesce(void *bp)
 static void *find_fit(size_t asize)
 {
 	/* First-fit search */
-	void *bp;
-
-	for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+	void *bp = heap_listp;
+	for (; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
 		if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
 			return bp;
 		}
 	}
 	return NULL; /* No fit */
-	#endif
 }
 
 /* used by mm_alloc */ 
@@ -238,4 +151,88 @@ static void place(void *bp, size_t asize)
 }
 
 
+/* 
+ *
+ *
+ *mm_init - initialize the malloc package. A 'prologue' and 'epilogue' block is used to account for edge cases
+ *
+ *
+ */
+int mm_init(void)
+{
+	/* create the initial empty heap, mem_sbrk returns a generic pointer to the start of the heap, so heap_listp currently holds it */
+	void *heap_listp = mem_sbrk(4*WSIZE);
+	if (heap_listp == (void *)–1){ return –1;}/* L: not entirely sure what this is checking for */
+	PUT(heap_listp, 0);
+	PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
+	PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
+	PUT(heap_listp + (3*WSIZE), PACK(0, 1));
+	heap_listp += (2*WSIZE); /* changes local variable holding heap pointer to the start of the actual heap */
+	/* extend the empty heap with a free block of CHUNKSIZE bytes */
+	if (extend_heap(CHUNKSIZE/WSIZE) == NULL) {return –1;}
+    return 0;
+}
 
+/* 
+ * mm_malloc - attemps to find a free block and then returns that pointer
+ *
+ */
+void *mm_malloc(size_t size)
+{
+	/* ignore spurious requests */
+	if (size == 0)
+		return NULL;
+	
+	size_t asize;	/* adjusted block size */
+	size_t extendsize;	/* amount to extend heap if no fit */
+	char *bp;
+	
+	/* adjust block size to include overhead and alignment reqs. */
+	if (size <= DSIZE) /* smaller than regular block size of payload */
+		asize = 2*DSIZE;
+	else /* larger than regular block size of payload */
+	asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE); /*L: first DSIZE is for footer and header, second DSIZE-1 is for extra space if unaligned
+	its -1 because size is at least 1 above DSIZE, division mods away unaligned 'remainder', then multiply back */
+	
+	/* Search the free list for a fit */
+	if ((bp = find_fit(asize)) != NULL) {
+		place(bp, asize);
+		return bp;
+	}
+
+	/* no fit found. get more memory and place the block */
+	extendsize = MAX(asize,CHUNKSIZE);
+	if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
+		return NULL;
+	place(bp, asize);
+	return bp;
+}
+
+/*
+ * mm_free - frees, and then coalesces, L: I don't think it checks whether that block is free or not but I guess it's not necessary
+ */
+void mm_free(void *bp)
+{
+	size_t size = GET_SIZE(HDRP(bp));
+	PUT(HDRP(bp), PACK(size, 0));
+	PUT(FTRP(bp), PACK(size, 0));
+	coalesce(bp);
+}
+
+/*
+ * mm_realloc - textbook does not have this implmentation, so we will have to work on it outselves
+ */
+void *mm_realloc(void *ptr, size_t size)
+{
+    /*ss */
+}
+
+/*
+* L: Heap Checker as per instructions, should be called at various points to check heap
+*/
+
+int mm_check(void) {
+	int x=1; /*initialize non-zero value, should return 0 if error, and print error messages before that
+	all checking code should go here, printing error message if heap not consistent, and changing x to 0 */
+	return x;
+}
